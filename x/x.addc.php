6f32b71cb7158fb3881ca23371dd769183f95e4f
itorr <itorrrrrr@me.com>
2015-01-04 16:57:47 +0800

    评论部分bug修正

--- x/x.addc.php
+++ x/x.addc.php
@@ -1,13 +1,12 @@
 <?php
-$m=array(
+
+foreach(array(
 	'author'=>'/^.+$/',
 	'mail'=>'/^\w+((-\w+)|(\.\w+))*\@[A-Za-z0-9]+((\.|-)[A-Za-z0-9]+)*\.[A-Za-z0-9]+$/',
 	'url'=>'/^(?:http[s]?:\/\/([\w-]+\.)+[\w-]+([\w-.\/?%&=]*)?|)$/',
-	'text'=>'/^.+$/',
+	'text'=>'/^.+$/s',
 	'pid'=>'/^[0-9]{1,10}$/',
-);
-
-foreach($m as $key=>$P){
+) as $key=>$P){
 	if(!isset($_POST[$key]))
 		err('恶意请求');
 	if(!preg_match($P,$_POST[$key]))
@@ -15,6 +14,11 @@ foreach($m as $key=>$P){
 
 	$r[$key]=$_POST[$key];
 }
+
+$p=$sql->getLine('SELECT pid FROM imouto_article WHERE pid='.$_POST['pid']);
+if(!isset($p['pid']))
+	err('您评论的文章不存在！');
+
 
 $r['created']=time();
 $r['agent']=$_SERVER['HTTP_USER_AGENT'];
@@ -30,10 +34,10 @@ foreach($r as $i=>$p){
 	//var_dump($p);
 	$w[]=is_string($p)?addslashes($p):$p;
 }
+
 $sql->runSql('INSERT INTO imouto_comments ('.implode(',',$n).') VALUES (\''.implode('\',\'',$w).'\');');
 
 //exit();
-
 
 
 //$r['agent']=$r['agent'];
