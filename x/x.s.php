bf70f6998a19512e958c432c9429011f2cae385f
卜卜口 <itorrrrrr@me.com>
2015-06-12 17:08:28 +0800

    后台 增加 搜索接口
    x/?a=s&q=关键字

--- x/x.s.php
+++ x/x.s.php
@@ -1,45 +1,21 @@
 <?php
-$m=array(
-	'data'=>'/^.+$/',
-	'hash'=>'',
-	'referer'=>'',
-);
+if(!isset($_GET['q']))
+	err();
 
-$_G=array_merge($_GET,$_POST);
+$q=addslashes($_GET['q']);
 
-//print_r($_G);
-foreach($m as $key=>$P){
-	if(!isset($_G[$key]))
-		err('恶意请求');
-	else if(!$P){
-
-	}else if(!preg_match($P,$_G[$key]))
-		err('您输入的'.$key.'不正确！');
-
-	$r[$key]=$_G[$key];
-}
+$r=$sql->getData('SELECT * FROM imouto_article WHERE title LIKE "%'.$q.'%" OR text LIKE  "%'.$q.'%" OR category LIKE "%'.$q.'%" LIMIT 0,30');
 
 
-if(preg_match('/卜卜口|prinexe/i',$r['data']))
-	exit();
+if($r)
+	$r=array_map(function($o){
+		$content=$o['text'];
+		$o['text']=($i=strpos($content,'<!--more-->'))!==false?substr($content,0,$i+11):$content;
 
-//$r['cookie']=$_COOKIE
-$r['url']=$_SERVER["HTTP_REFERER"];
-$r['unix']=time();
-$r['agent']=$_SERVER['HTTP_USER_AGENT'];
-$r['ip']=$_SERVER['REMOTE_ADDR'];
+		$o['text']=strip_tags($o['text']);
+		$o['text']=mb_substr($o['text'],0,100).'…';
+		$o['text']=str_replace(PHP_EOL,' ',$o['text']); 
+		unset($o['modified']);
 
-
-//var_dump($r);
-
-$n=array();
-$w=array();
-foreach($r as $i=>$p){
-	$n[]=$i;
-	//var_dump($p);
-	$w[]=is_string($p)?addslashes($p):$p;
-}
-
-$sql->runSql('INSERT INTO imouto_stats ('.implode(',',$n).') VALUES (\''.implode('\',\'',$w).'\');');
-
-exit();
+		return $o;
+	},$r);
